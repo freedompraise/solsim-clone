e8dfb89a0798229cc301167615c0c3342e6c2125
Praise Freedom Dike <dikepraise119@gmail.com>
2025-01-01 22:36:21 +0100

    update Footer component to wrap social media icons in links for improved accessibility and navigation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,22 +12,33 @@ const Footer = () => {
       <div class="flex justify-center gap-4">
         <div class="bg-black w-10 h-10 rounded-full flex items-center justify-center">
           <i class="text-white text-2xl">
-            <FontAwesomeIcon icon={faTwitter} />
+            <a href="https://twitter.com/freedom_praise" target="_blank">
+              <FontAwesomeIcon icon={faTwitter} />
+            </a>
           </i>
         </div>
         <div class="bg-black w-10 h-10 rounded-full flex items-center justify-center">
           <i class="text-white text-2xl">
-            <FontAwesomeIcon icon={faFacebook} />
+            <a
+              href="https://www.facebook.com/profile.php?id=100009170232211"
+              target="_blank"
+            >
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
           </i>
         </div>
         <div class="bg-black w-10 h-10 rounded-full flex items-center justify-center">
           <i class="text-white text-2xl">
-            <FontAwesomeIcon icon={faGithub} />
+            <a href="https://github.com/freedompraise" target="_blank">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
           </i>
         </div>
         <div class="bg-black w-10 h-10 rounded-full flex items-center justify-center">
           <i class="text-white text-2xl">
-            <FontAwesomeIcon icon={faLinkedin} />
+            <a href="https://www.linkedin.com/in/praise-dike" target="_blank">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
           </i>
         </div>
       </div>

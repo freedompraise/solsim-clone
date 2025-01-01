b25fedbeb6484d93331da0c62dd645d5292dc6b7
Praise Freedom Dike <dikepraise119@gmail.com>
2025-01-01 12:41:15 +0100

    update ProductCard component for improved layout and styling; enhance logo size and card title responsiveness

--- src/components/Product/ProductCard.jsx
+++ src/components/Product/ProductCard.jsx
@@ -2,18 +2,23 @@ import logo from "../../assets/logo.png";
 
 const ProductCard = () => {
   return (
-    <div className="bg-white shadow-lg rounded-lg lg:mx-8 lg:p-8 sm:p-4 lg:my-8 flex flex-col items-center">
+    <div className="bg-white shadow-lg rounded-lg lg:mx-8 p-8 lg:my-8 flex flex-col items-center h-600 min-h-screen">
       {/* Gradient Background */}
-      <div className="relative w-full h-48 rounded-lg bg-gradient-to-r from-purple-500 to-[#CC3DFF] flex items-center justify-center">
+      <div
+        className="relative rounded-lg bg-gradient-to-tl from-[#CC3DFF] to-purple-500 p-6 flex flex-col justify-center w-full max-w-md"
+        style={{
+          height: "293px",
+        }}
+      >
         {/* Logo */}
         <img
           src={logo}
           alt="SOLSIM Logo"
-          className="absolute top-4 left-4 w-12 h-12"
+          className="absolute top-6 left-6 w-16 h-16"
         />
 
         {/* Card Title */}
-        <h2 className="text-white text-xl font-semibold">
+        <h2 className="text-white text-2xl font-semibold text-center mt-32">
           North America SOLSIM
         </h2>
       </div>
